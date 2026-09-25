28d696227124c0392e4ef312cb81b76a98b1dfb7
rraghul404 <rraghul404@gmail.com>
2026-09-25 17:09:35 +0530

    last commit

--- amazon.spec.js
+++ amazon.spec.js
@@ -1,10 +1,10 @@
 import {test} from '@playwright/test'
- test('Iphone',async({page})=>{
+ test('Iphone 17',async({page})=>{
     await page.goto("https://www.amazon.in");
-    await page.locator ('//input[@id="twotabsearchtextbox"]').fill('iphone');
+    await page.locator ('//input[@id="twotabsearchtextbox"]').fill('iphone 17');
     await page.locator ('//input[@id="nav-search-submit-button"]').click('Enter');
     const iphone = page.locator (`//span[@data-component-type="s-search-results"]/div/div[@role="listitem"]/a[@aria-label="4.6 out of 5 stars, rating details"]`)
-                        .filter({ hasText: 'iPhone' }).allTextContents();
+                        .filter({ hasText: 'iPhone 17' }).allTextContents();
 
     console.log('iphone');
 
